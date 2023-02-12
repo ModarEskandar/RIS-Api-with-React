3bb76e3593ece076705e9fc095bcea6c901069ed
AlexandaerModaric <eskandar.alassad@gmail.com>
2023-02-12 23:44:34 +0300

    react front end

---
+++ myApi/Configurations/MapperIntializer.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Data.DTOs;
+using Data.Models;
+
+namespace Configurations
+{
+    class MapperInitializer : Profile
+    {
+        public MapperInitializer()
+        {
+            CreateMap<Patient, PatientDTO>().ReverseMap();
+            CreateMap<Patient, CreatePatientDTO>().ReverseMap();
+            CreateMap<RadOrder, RadOrderDTO>().ReverseMap();
+            CreateMap<RadOrder, CreatePatientDTO>().ReverseMap();
+        }
+
+    }
+}
